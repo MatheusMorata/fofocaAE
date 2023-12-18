f6284972a684c695fea5fa0b77be303fc0f6baec
MatheusMorata <matlucar09@hotmail.com>
2023-12-18 20:19:37 -0300

    feat main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import comandos as c
 import os 
-
+canal = "UERJ"
 key = ""
 op = 0
 def menu():
@@ -25,28 +25,34 @@ while(op != '8'):
         print("Sua identidade: "+key)
     elif(op == '2'):
         os.system("clear")
-        msgs = c.leitura_msgs()
+        msgs = c.leitura_msgs(canal)
         for i in range(1,len(msgs)):
-            print("Fofoca: ",i," - ",c.descript_msg(msgs[i]))
+            print("Fofoca: ",i," - ",c.descript_msg(msgs[i],canal))
     elif(op == '3'):
         os.system("clear")
         if (key != ""):
             fofoca = str(input("Conte-me uma fofoca: "))
-            c.post(fofoca,key)
+            c.post(fofoca,key,canal)
             os.system("clear")
             print("Fofoca enviada")
         else:
             print("Crie uma identidade para comecar a fofocar")
     elif(op == '4'):
        os.system("clear")
-       num = str(input("Digite o numero da fofoca: "))
-        
+       print("Tamo no 4")
+
     elif(op == '5'):
+       os.system("clear")
+       print("Tamo no 5")
 
     elif(op == '6'):
+        os.system("clear")
+        print("Tamo no 6")
 
     elif(op == '7'):
-
+       os.system("clear")
+       print("Tamo no 7")
+       
     elif(op == '8'):
         os.system("clear")
         print("Finalizando...")
